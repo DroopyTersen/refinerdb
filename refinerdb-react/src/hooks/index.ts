2335bc64445aa129e837053c7c4ea47a02d81f41
Andrew Petersen <andrew.petersen15@gmail.com>
2020-01-25 16:06:42 -0600

    cleaned up the hooks export

---
+++ refinerdb-react/src/hooks/index.ts
@@ -0,0 +1,7 @@
+export { default as useRefinerDB } from "./useRefinerDB";
+export { default as useIndexes } from "./useIndexes";
+export { default as useIndexState } from "./useIndexState";
+export { default as useQueryResult } from "./useQueryResult";
+export { default as useRefiner } from "./useRefiner";
+export { default as useFilter } from "./useFilter";
+export { default as useCriteria } from "./useCriteria";
--- refinerdb-react/src/index.tsx
+++ refinerdb-react/src/index.tsx
@@ -1,11 +1,5 @@
 export { default as RefinerDBProvider } from "./RefinerDBProvider";
-export { default as useRefinerDB } from "./hooks/useRefinerDB";
-export { default as useIndexes } from "./hooks/useIndexes";
-export { default as useIndexState } from "./hooks/useIndexState";
-export { default as useQueryResult } from "./hooks/useQueryResult";
-export { default as useRefiner } from "./hooks/useRefiner";
-export { default as useFilter } from "./hooks/useFilter";
-export { default as useCriteria } from "./hooks/useCriteria";
-
+export * from "./hooks";
 export * from "./components/refinerControls";
 export * from "./components/results";
+export * from "refinerdb";
