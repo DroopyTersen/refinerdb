b54b4b6ec42a1ade9fca9761534fbbf7f5b99bed
Andrew Petersen <andrew.petersen15@gmail.com>
2021-11-06 18:06:52 -0600

    exposed hooks

--- packages/refinerdb-react/src/hooks/index.ts
+++ packages/refinerdb-react/src/hooks/index.ts
@@ -1,3 +1,8 @@
+export * from "./useTextRefiner";
+export * from "./useDateRangeRefiner";
+export * from "./useNumberRangeRefiner";
+export * from "./useMultiselectRefiner";
+
 export { default as useRefinerDB } from "./useRefinerDB";
 export { default as useIndexes } from "./useIndexes";
 export { default as useIndexState } from "./useIndexState";
--- packages/refinerdb/src/index.ts
+++ packages/refinerdb/src/index.ts
@@ -1,9 +1,9 @@
 import RefinerDB from "./RefinerDB";
 
 export * from "./interfaces";
-
 export default RefinerDB;
 export { default as binarySearch } from "./helpers/binarySearch";
 export { default as query } from "./transactions/query/query";
 export * from "./transactions/indexItems";
+
 export * from "./stores/BasePersistedStore";
